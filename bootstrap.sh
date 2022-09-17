d6c4bc437f4602ee631002a438b694283aaf5536
Vigilans <vigilans@foxmail.com>
2022-09-17 20:54:08 +0800

    Install necessary packages in bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,17 @@
 
 export SHELL_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"; cd "$SHELL_HOME"
 
+# Setup necessary packages
+if command -v pacman &> /dev/null; then
+    sudo pacman -S wget git tar unzip
+elif command -v apt-get &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get install wget git tar unzip
+else
+    echo "Package manager not supported for now"
+    return 1
+fi
+
 # Setup ZI
 mkdir -p ~/.zi && git clone git@github.com:z-shell/zi.git ~/.zi/bin
 
