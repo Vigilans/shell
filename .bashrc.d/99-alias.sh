56b9145d6aaa93af4ca4ee67cd644e102040424d
Vigilans <vigilans@foxmail.com>
2022-12-08 16:10:46 +0800

    Move alias import to the end of shell rc script

--- .bashrc.d/99-alias.sh
+++ .bashrc.d/99-alias.sh
@@ -1,7 +1,4 @@
-# some more ls aliases
-#alias ll='ls -l'
-#alias la='ls -A'
-#alias l='ls -CF'
+# alias.sh: setup scripts for shell aliases.
 
 # Alias definitions.
 # You may want to put all your additions into a separate file like
@@ -9,4 +6,13 @@
 # See /usr/share/doc/bash-doc/examples in the bash-doc package.
 if [ -f ~/.bash_aliases ]; then
     . ~/.bash_aliases
+fi
+
+
+# Load shell aliases
+if [ -d $SHELL_CONFIG_HOME/aliases ]; then
+    for alias in $SHELL_CONFIG_HOME/aliases/*.sh; do
+        test -r "$alias" && . "$alias"
+    done
+    unset alias
 fi
