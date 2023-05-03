31be8096d20072f19a0e3414b05f26e26b863e9a
Vigilans <vigilans@foxmail.com>
2023-05-04 03:39:55 +0800

    Add PDFVIEWER env support when in vscode

--- .initrc.d/01-env.sh
+++ .initrc.d/01-env.sh
@@ -1,9 +1,17 @@
-if [ "$TERM_PROGRAM" = "vscode" ]; then
-    export EDITOR="code --wait"
-elif type nano 1>/dev/null 2>&1; then
-    export EDITOR="nano -l"
-elif type vim 1>/dev/null 2>&1; then
-    export EDITOR=vim
-else
-    export EDITOR=vi
+if [ -z "$EDITOR" ]; then
+    if [ "$TERM_PROGRAM" = "vscode" ]; then
+        export EDITOR="code --wait"
+    elif type nano 1>/dev/null 2>&1; then
+        export EDITOR="nano -l"
+    elif type vim 1>/dev/null 2>&1; then
+        export EDITOR=vim
+    else
+        export EDITOR=vi
+    fi
+fi
+
+if [ -z "$PDFVIEWER" ]; then
+    if [ "$TERM_PROGRAM" = "vscode" ]; then
+        export PDFVIEWER="code --reuse-window"
+    fi
 fi
