9e8164b0c2334082c574d018d7e0d1aba4fadd66
Vigilans <vigilans@foxmail.com>
2023-05-31 23:27:37 +0800

    Add quotes to cuda and intel profile

--- profiles/system/cuda.sh
+++ profiles/system/cuda.sh
@@ -1,4 +1,4 @@
 if [ -n "$CUDA_HOME" ]; then
-    export PATH=$PATH:$CUDA_HOME/bin
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_HOME/lib64
+    export PATH="$PATH:$CUDA_HOME/bin"
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$CUDA_HOME/lib64"
 fi
