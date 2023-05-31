037b8978812636ac867ad0fc8a6d1ee8faae68d9
Vigilans <vigilans@foxmail.com>
2023-05-31 23:27:25 +0800

    Add LLVM profile

---
+++ profiles/system/llvm.sh
@@ -0,0 +1,12 @@
+if ! command -v clang &> /dev/null && [ -z "$LLVM_HOME" ]; then
+    for __llvm_lib_dir in $(printf "%s\n" /usr/lib/llvm-* | sort -r -n -t '-' -k 2); do
+        export LLVM_HOME="$__llvm_lib_dir"
+        break
+    done
+    unset __llvm_lib_dir
+fi
+
+if [ -n "$LLVM_HOME" ]; then
+    export PATH=$PATH:$LLVM_HOME/bin
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LLVM_HOME/lib
+fi
