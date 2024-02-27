3d030a89d922acfec8abde5f1bc8a600d645b153
Vigilans <vigilans@foxmail.com>
2024-02-27 14:38:34 +0800

    Use relative path for symlinks

--- bootstrap.sh
+++ bootstrap.sh
@@ -24,7 +24,7 @@ mkdir -p ~/.zi && git clone git@github.com:z-shell/zi.git ~/.zi/bin
 mkdir -p ~/.config/zi && wget https://raw.githubusercontent.com/z-shell/zi-src/main/lib/zsh/init.zsh -O ~/.config/zi/init.zsh
 
 # Setup Shell
-mkdir -p ~/.config && ln -snf "$SHELL_HOME" ~/.config/shell
+mkdir -p ~/.config && ln -snf $(realpath --relative-to="$HOME/.config" "$SHELL_HOME")  "$HOME/.config/shell"
 for dotfile in $(/bin/ls -A "$SHELL_HOME/.initrc.d/dotfiles"); do
-    ln -snf "$SHELL_HOME/.initrc.d/dotfiles/$dotfile" ~/$dotfile
+    ln -snf $(realpath --relative-to="$HOME" "$SHELL_HOME/.initrc.d/dotfiles/$dotfile") "$HOME/$dotfile"
 done
