4bb32aab29f60198e815be4b06b937cb36fabdc4
Vigilans <vigilans@foxmail.com>
2024-12-25 18:22:36 +0800

    Modify rust profile to not enforce default value
    Signed-off-by: Vigilans <vigilans@foxmail.com>

--- profiles/system/rust.sh
+++ profiles/system/rust.sh
@@ -1,15 +1,9 @@
-if [ -z "$RUSTUP_HOME" ]; then
-    export RUSTUP_HOME="$HOME/.rustup"
-fi
+if [ -n "$CARGO_HOME" ]; then
+    if [ -d "$CARGO_HOME/bin" ]; then
+        export PATH="$CARGO_HOME/bin:$PATH"
+    fi
 
-if [ -z "$CARGO_HOME" ]; then
-    export CARGO_HOME="$HOME/.cargo"
-fi
-
-if [ -d "$CARGO_HOME/bin" ]; then
-    export PATH="$CARGO_HOME/bin:$PATH"
-fi
-
-if [ -r "$CARGO_HOME/env" ]; then
-    . $CARGO_HOME/env
+    if [ -r "$CARGO_HOME/env" ]; then
+        . $CARGO_HOME/env
+    fi
 fi
