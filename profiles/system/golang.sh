ee9b386b33ba1ec06a5258aa3d3f21ccd8cae111
Vigilans <vigilans@foxmail.com>
2024-12-25 16:07:32 +0800

    Comment out setting GROOT and GOPATH
    Signed-off-by: Vigilans <vigilans@foxmail.com>

--- profiles/system/golang.sh
+++ profiles/system/golang.sh
@@ -1,9 +1,9 @@
-if [ -z "$GOROOT" ] || [ -z "$GOPATH" ]; then
-    if command -v go &> /dev/null; then
-        export GOROOT="$(go env GOROOT)"
-        export GOPATH="$(go env GOPATH)"
-    fi
-fi
+# if [ -z "$GOROOT" ] || [ -z "$GOPATH" ]; then
+#     if command -v go &> /dev/null; then
+#         export GOROOT="$(go env GOROOT)"
+#         export GOPATH="$(go env GOPATH)"
+#     fi
+# fi
 
 if [ -n "$GOPATH" ]; then
     export PATH=$GOPATH/bin:$PATH
