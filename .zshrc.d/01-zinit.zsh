bb8b28ebf4be5e5b6e55a558fa68828fec3f030e
Vigilans <vigilans@foxmail.com>
2025-02-08 00:08:56 +0800

    Fix zinit snippet completion generation
    Modify completion snippet generation to use command substitution for more robust shell execution

--- .zshrc.d/01-zinit.zsh
+++ .zshrc.d/01-zinit.zsh
@@ -154,7 +154,7 @@ function zinit_snippet_completion_from_stdin() {
     local command=$1
     local completion_cmdline=$2
     zinit ice wait lucid id-as"_$command" as'completion' blockf atpull!'%atclone' atclone"
-        $completion_cmdline > _$command"
+        { $completion_cmdline } > _$command"
     zinit snippet /dev/null
 }
 
