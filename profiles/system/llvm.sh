0fe614223ae7d3d23ea211888761e317dc966a7b
Vigilans <vigilans@foxmail.com>
2025-08-01 08:07:51 +0000

    Suppress match not found error in llvm profile

--- profiles/system/llvm.sh
+++ profiles/system/llvm.sh
@@ -1,5 +1,5 @@
 if ! command -v clang &> /dev/null && [ -z "$LLVM_HOME" ]; then
-    for __llvm_lib_dir in $(printf "%s\n" /usr/lib/llvm-* | sort -r -n -t '-' -k 2); do
+    for __llvm_lib_dir in $(printf "%s\n" /usr/lib/llvm-* 2>/dev/null | sort -r -n -t '-' -k 2); do
         export LLVM_HOME="$__llvm_lib_dir"
         break
     done
