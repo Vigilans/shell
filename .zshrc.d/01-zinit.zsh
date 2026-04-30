6792ba9863b9a99af4c891e8b40fdf47ead18090
Vigilans <vigilans@foxmail.com>
2026-04-30 18:14:39 +0800

    Drop gh bpick override and let zinit auto-detect
    bpick"gh_*.tar.gz" pinned to tar.gz, but gh ships macOS as .zip and
    linux as .tar.gz, so on macOS zinit fell through to the linux tar.gz
    build. Removing the override lets zinit's built-in uname-based picker
    choose the right asset (macOS_arm64.zip on this host, etc.).

--- .zshrc.d/01-zinit.zsh
+++ .zshrc.d/01-zinit.zsh
@@ -110,7 +110,7 @@ zinit ice wait lucid from'gh-r' id-as as'null' bpick"*$(HOST_TRIPLET_APPLE_USE_I
 zinit load sharkdp/vivid
 
 # `gh` github cli
-zinit ice wait lucid from'gh-r' id-as'gh' as'completion' bpick"gh_*.tar.gz" mv'gh* release' atpull'%atclone' atclone'
+zinit ice wait lucid from'gh-r' id-as'gh' as'completion' mv'gh* release' atpull'%atclone' atclone'
     ln -svf $PWD/release/bin/gh $ZPFX/bin
     ln -svf $PWD/release/share/man/man1/* $ZPFX/man/man1
     gh completion -s zsh > _gh'
